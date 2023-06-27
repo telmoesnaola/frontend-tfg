b01e5644c640de82cd08841bde42913fce80111d
telmoesnaola <telmoesnaolaa@gmail.com>
2023-06-27 16:15:18 +0200

    prueba grafana

--- src/Home.js
+++ src/Home.js
@@ -21,12 +21,12 @@ const Home = ()=>{
             <Row className='d-flex justify-content-center align-items-center' >
                 <Card bg="dark" className='bg-dark text-white my-5 p-1' style={{borderRadius: '1rem', maxWidth: '48%'}}>
                     <Card.Body className='d-flex flex-column align-items-center mx-auto w-100'>
-                        <iframe src="http://localhost:3000/dashboard-solo/new?orgId=1&panelId=4" width="100%" height="300" style={{borderRadius: '1rem',}}></iframe>
+                        <iframe src="http://ec2-34-251-157-57.eu-west-1.compute.amazonaws.com:3000/d-solo/cdedc882-8652-49e6-bd4e-34b9d15518c2/home?orgId=1&from=1685283099897&to=1687875099897&panelId=123124" width="100%" height="300" style={{borderRadius: '1rem',}}></iframe>
                     </Card.Body>
                 </Card>
                 <Card bg="dark" className='bg-dark text-white my-5 p-1' style={{borderRadius: '1rem', maxWidth: '48%'}}>
                     <Card.Body className='d-flex flex-column align-items-center mx-auto w-100'>
-                        <iframe src="http://localhost:3000/d-solo/EyGvPlL4z/apicount?orgId=1&panelId=123125" width="100%" height="300" style={{borderRadius: '1rem',}}></iframe> 
+                        <iframe src="http://localhost:3000/d-solo/cdedc882-8652-49e6-bd4e-34b9d15518c2/home?orgId=1&from=1685283099897&to=1687875099897&panelId=123124" width="100%" height="300" style={{borderRadius: '1rem',}}></iframe> 
                     </Card.Body>
                 </Card>
             </Row>
